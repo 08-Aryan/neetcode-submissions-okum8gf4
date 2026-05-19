46016160652b9d75201c0ca269ea7b8e307afa48
08-Aryan <123479305+08-Aryan@users.noreply.github.com>
2026-05-19 17:01:47 +0530

    Add: is-anagram - submission-1

---
+++ Data Structures & Algorithms/is-anagram/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // sort both TC = > O(nlogn) SC= 1
+        // Frequency of all elemets 
+        vector<int> freq(26,0);
+        for(auto &ch:s){
+            int x = ch - 'a';
+            freq[x]++;
+        }
+        for(auto &ch:t){
+            int y = ch - 'a';
+            freq[y]--;
+        }
+        for(int i=0;i<26;i++){
+            if(freq[i]!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+};
